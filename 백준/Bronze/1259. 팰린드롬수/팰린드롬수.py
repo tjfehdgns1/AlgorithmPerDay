3a734f1a8ceba6cc30d05a44d5ea72489c933564
SeolMan <tjfehdgns@gmail.com>
2023-10-28 18:32:22 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,11 @@
+from sys import stdin
+
+while True:
+    nums = input().strip()
+
+    if nums == '0':
+        break
+    if nums == nums[::-1]:
+        print('yes')
+    else:
+        print('no')
