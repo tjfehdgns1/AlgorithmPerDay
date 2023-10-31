adfc1f46256bf970e69a91e58c8232deec133111
SeolMan <tjfehdgns@gmail.com>
2023-10-31 21:39:00 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,25 @@
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+
+a = [input().strip() for _ in range(n)]
+
+for b in a:
+    d = 0
+    for i, c in enumerate(b):
+        if c == '(':
+            d += 1
+        elif c == ')':
+            d -= 1
+    
+        if d < 0:
+            print('NO')
+            break
+    else:
+        if d == 0:
+            print('YES')
+        else:
+            print('NO')
+            
+
