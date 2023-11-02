5b2c72938e250769667cfbad76a4eb16d9f8579d
SeolMan <tjfehdgns@gmail.com>
2023-11-02 23:26:02 +0900

    [Silver IV] Title: 덱, Time: 72 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,42 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
+
+n = int(input())
+
+a = deque()
+
+for i in range(n):
+    b = input().split()
+    match b[0]:
+        case 'push_front':
+            a.append(int(b[1]))
+        case 'push_back':
+            a.appendleft(int(b[1]))
+        case 'pop_front':
+            if a:
+                print(a.pop())
+            else:
+                print(-1)
+        case 'pop_back':
+            if a:
+                print(a.popleft())
+            else:
+                print(-1)
+        case 'size':
+            print(len(a))
+        case 'empty':
+            if a:
+                print(0)
+            else:
+                print(1)
+        case 'front':
+            if a:
+                print(a[-1])
+            else:
+                print(-1)
+        case 'back':
+            if a:
+                print(a[0])
+            else:
+                print(-1)
