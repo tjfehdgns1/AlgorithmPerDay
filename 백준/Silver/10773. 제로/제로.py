00c317e73ecca86f2033e23a6109ad81c3e50451
SeolMan <tjfehdgns@gmail.com>
2023-11-03 16:22:39 +0900

    [Silver IV] Title: 제로, Time: 80 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,16 @@
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+b = []
+for _ in range(n):
+    a = int(input())
+    if a != 0:
+        b.append(a)
+    else:
+        b.pop()
+
+if b:
+    print(sum(b))
+else:
+    print(0)
