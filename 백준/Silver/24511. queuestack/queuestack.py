3dcd794cb2c819c5484e5c3174f9b71efd2fe062
SeolMan <tjfehdgns@gmail.com>
2023-11-03 21:34:00 +0900

    [Silver III] Title: queuestack, Time: 216 ms, Memory: 48268 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.py
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -2,18 +2,18 @@ import sys
 from collections import deque
 
 n = int(sys.stdin.readline())
-list_a = list(map(int, sys.stdin.readline().split()))  # 0 1 1 0 (0 = queue, 1 = stack)
-list_b = list(map(int, sys.stdin.readline().split()))  # 1 2 3 4
+a = list(map(int, sys.stdin.readline().split()))
+b = list(map(int, sys.stdin.readline().split()))
 
 m = int(sys.stdin.readline())
-list_c = list(map(int, sys.stdin.readline().split()))
+c = list(map(int, sys.stdin.readline().split()))
 
-res = deque()
+r = deque()
 
-for qs in range(n):
-    if list_a[qs] == 0:
-        res.appendleft(list_b[qs])
+for j in range(n):
+    if a[j] == 0:
+        r.appendleft(b[j])
         
 for i in range(m):
-    res.append(list_c[i])
-    print(res.popleft(), end=' ')
+    r.append(c[i])
+    print(r.popleft(), end=' ')
