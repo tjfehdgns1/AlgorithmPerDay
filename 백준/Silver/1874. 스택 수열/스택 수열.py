30369ffece91b68c1b2e2d489fe6ff4f04d92741
SeolMan <tjfehdgns@gmail.com>
2023-11-06 10:17:02 +0900

    [Silver II] Title: 스택 수열, Time: 3796 ms, Memory: 36560 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+given = [int(input()) for _ in range(n)]
+stack = []
+result = []
+current = 1
+flag = False
+for i in given:
+    while current <= i:
+        result.append('+')
+        stack.append(current)
+        current += 1
+
+    if stack[-1] == i:
+        result.append('-')
+        stack.pop()
+
+    else:
+        print('NO')
+        flag = True
+        break
+
+if not flag:
+    for i in result:
+        print(i)
