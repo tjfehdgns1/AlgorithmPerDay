48c41526b599371ee9b02063be7facca41047955
SeolMan <tjfehdgns@gmail.com>
2023-11-06 10:54:01 +0900

    [Silver IV] Title: solved.ac, Time: 160 ms, Memory: 35868 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+
+input = stdin.readline
+n = int(input())
+if n == 0:
+    print(0)
+    exit()
+
+a = sorted(int(input()) for _ in range(n))
+
+
+def round5(n):
+    if n - int(n) >= 0.5:
+        return int(n) + 1
+    else:
+        return int(n)
+
+
+c = round5(n * 0.15)
+
+b = a[c:n-c]
+
+print(round5(sum(b) / len(b)))
