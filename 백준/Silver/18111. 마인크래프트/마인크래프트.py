875fc6f18560acfdc5a0b4496dd39d88fe9f03c2
SeolMan <tjfehdgns@gmail.com>
2023-11-09 16:17:34 +0900

    [Silver II] Title: 마인크래프트, Time: 816 ms, Memory: 112876 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+input = stdin.readline
+n, m, b = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+time = float('inf')
+height = 0
+
+for i in range(257):
+    use = 0
+    take = 0
+    for x in range(n):
+        for y in range(m):
+            if grid[x][y] > i:
+                take += grid[x][y] - i
+            else:
+                use += i - grid[x][y]
+
+    if use > take + b:
+        continue
+
+    t = take * 2 + use
+
+    if t <= time:
+        time = t
+        height = i
+
+print(time, height, sep=' ')
