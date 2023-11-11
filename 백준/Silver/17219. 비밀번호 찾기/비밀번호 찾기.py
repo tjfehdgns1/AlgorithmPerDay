617e5b3f326b6fdddbc79d6564e833a3cadbaeda
SeolMan <tjfehdgns@gmail.com>
2023-11-11 22:07:48 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 244 ms, Memory: 49040 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,12 @@
+import sys
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split()) 
+dic = dict()
+for _ in range(n):
+    a, b = input().rstrip().split()
+    dic[a] = b
+
+for _ in range(m):
+    print(dic[input().rstrip()])
