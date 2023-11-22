8e47eaa3250690ba7b17c95f6a50b91926925e68
SeolMan <tjfehdgns@gmail.com>
2023-11-22 21:58:29 +0900

    [Silver II] Title: 색종이 만들기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,38 +1,31 @@
 import sys
 
-def solution(grid, x, y, N):
-    color = grid[x][y]
-    result = [0, 0]
+N = int(sys.stdin.readline())
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
+result = []
+
+def solution(x, y, N):
+    color = paper[x][y]
     for i in range(x, x + N):
         for j in range(y, y + N):
-            if color != grid[i][j]:
-                # Accumulate results from recursive calls
-                result[0] += solution(grid, x, y, N//2)[0] + solution(grid, x, y + N//2, N//2)[0]
-                result[1] += solution(grid, x, y, N//2)[1] + solution(grid, x, y + N//2, N//2)[1]
-                result[0] += solution(grid, x + N//2, y, N//2)[0] + solution(grid, x + N//2, y + N//2, N//2)[0]
-                result[1] += solution(grid, x + N//2, y, N//2)[1] + solution(grid, x + N//2, y + N//2, N//2)[1]
-                return result
-
+            if color != paper[i][j]:
+                solution(x, y, N // 2)
+                solution(x, y + N // 2, N // 2)
+                solution(x + N // 2, y, N // 2)
+                solution(x + N // 2, y + N // 2, N // 2)
+                return
     if color == 0:
-        result[0] += 1
+        result.append(0)
     else:
-        result[1] += 1
-    
-    return result
+        result.append(1)
 
 def main():
-    input_func = sys.stdin.readline
-    n = int(input_func())
-    grid = []
-
-    for i in range(n):
-        grid.append(list(map(int, input_func().split())))
-
-    zero, one = solution(grid, 0, 0, n)
-
-    print(zero)
-    print(one)
+    global result
+    global N
+    solution(0, 0, N)
+    print(result.count(0))
+    print(result.count(1))
 
 if __name__ == "__main__":
     main()
