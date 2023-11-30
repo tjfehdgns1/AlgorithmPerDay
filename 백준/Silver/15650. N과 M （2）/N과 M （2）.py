d5f4e57e58fec8896db7f721402f5486412c9c72
SeolMan <tjfehdgns@gmail.com>
2023-11-30 22:33:19 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,22 @@
+def backtracking(N, M, result, visited, start):
+    if len(result) == M:
+        print(' '.join(map(str, result)))
+        return
+    
+    for i in range(start, N + 1):
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
+            backtracking(N, M, result, visited, i + 1)
+            visited[i] = False
+            result.pop()
+
+def main():
+    N, M = map(int, input().split())
+    visited = [False] * (N + 1)
+    result = []
+
+    backtracking(N, M, result, visited, 1)
+
+if __name__ == "__main__":
+    main()
