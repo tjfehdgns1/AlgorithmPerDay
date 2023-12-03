2752326c7fbebfd6ad2a6dfac9518e047f705b5d
SeolMan <tjfehdgns@gmail.com>
2023-12-03 14:16:56 +0900

    [Silver II] Title: A → B, Time: 88 ms, Memory: 34000 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+
+
+def bfs(start, target):
+    q = deque([(start, 1)])
+    while q:
+        current, count = q.popleft()
+
+        if current == target:
+            print(count)
+            return
+        
+        if current * 2 <= target:
+            q.append((current * 2, count + 1))
+
+        if current * 10 + 1 <= target:
+            q.append((current * 10 + 1, count + 1))
+    print(-1)
+
+def main():
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+
+    bfs(n, m)
+
+        
+if __name__ == "__main__":
+    main()
